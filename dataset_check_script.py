8786b1965278ec6efb3099c459ff5df51f0711c9
Firizki Emsa <muhammad.firizki@wartek.belajar.id>
2023-07-10 11:29:00 +0800

    Bump script

--- dataset_check_script.py
+++ dataset_check_script.py
@@ -58,183 +58,183 @@ def plot_img(image_name):
 
     plt.show()
 
-plot_img("3309.png")
+# plot_img("3309.png")
 
-# _classes = np.insert(classes, 0, "background", axis=0)        # adding a background class for Faster R-CNN
-# class_to_int = {_classes[i] : i for i in range(len(_classes))}
-# int_to_class = {i : _classes[i] for i in range(len(_classes))}
+_classes = np.insert(classes, 0, "background", axis=0)        # adding a background class for Faster R-CNN
+class_to_int = {_classes[i] : i for i in range(len(_classes))}
+int_to_class = {i : _classes[i] for i in range(len(_classes))}
 
-# class FaceMaskDetectionDataset(Dataset):
+class FaceMaskDetectionDataset(Dataset):
     
-#     def __init__(self, dataframe, image_dir, mode = 'train', transforms = None):
+    def __init__(self, dataframe, image_dir, mode = 'train', transforms = None):
         
-#         super().__init__()
+        super().__init__()
         
-#         self.image_names = dataframe["name"].unique()
-#         self.df = dataframe
-#         self.image_dir = image_dir
-#         self.transforms = transforms
-#         self.mode = mode
+        self.image_names = dataframe["name"].unique()
+        self.df = dataframe
+        self.image_dir = image_dir
+        self.transforms = transforms
+        self.mode = mode
         
-#     def __getitem__(self, index: int):
+    def __getitem__(self, index: int):
         
-#         #Retrive Image name and its records (x1, y1, x2, y2, classname) from df
-#         image_name = self.image_names[index]
-#         records = self.df[self.df["name"] == image_name]
+        #Retrive Image name and its records (x1, y1, x2, y2, classname) from df
+        image_name = self.image_names[index]
+        records = self.df[self.df["name"] == image_name]
         
-#         #Loading Image
-#         image = cv2.imread(self.image_dir + image_name, cv2.IMREAD_COLOR)
-#         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
-#         image /= 255.0
+        #Loading Image
+        image = cv2.imread(self.image_dir + image_name, cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image /= 255.0
         
-#         if self.mode == 'train':
+        if self.mode == 'train':
             
-#             #Get bounding box co-ordinates for each box
-#             boxes = records[['x1', 'y1', 'x2', 'y2']].values
+            #Get bounding box co-ordinates for each box
+            boxes = records[['x1', 'y1', 'x2', 'y2']].values
 
-#             #Getting labels for each box
-#             temp_labels = records[['classname']].values
-#             labels = []
-#             for label in temp_labels:
-#                 label = class_to_int[label[0]]
-#                 labels.append(label)
+            #Getting labels for each box
+            temp_labels = records[['classname']].values
+            labels = []
+            for label in temp_labels:
+                label = class_to_int[label[0]]
+                labels.append(label)
 
-#             #Converting boxes & labels into torch tensor
-#             boxes = torch.as_tensor(boxes, dtype=torch.float32)
-#             labels = torch.as_tensor(labels, dtype=torch.int64)
+            #Converting boxes & labels into torch tensor
+            boxes = torch.as_tensor(boxes, dtype=torch.float32)
+            labels = torch.as_tensor(labels, dtype=torch.int64)
 
-#             #Creating target
-#             target = {}
-#             target['boxes'] = boxes
-#             target['labels'] = labels
+            #Creating target
+            target = {}
+            target['boxes'] = boxes
+            target['labels'] = labels
 
-#             #Transforms
-#             if self.transforms:
-#                 image = self.transforms(image)
+            #Transforms
+            if self.transforms:
+                image = self.transforms(image)
 
 
-#             return image, target, image_name
+            return image, target, image_name
         
-#         elif self.mode == 'test':
+        elif self.mode == 'test':
 
-#             if self.transforms:
-#                 image = self.transforms(image)
+            if self.transforms:
+                image = self.transforms(image)
 
-#             return image, image_name
+            return image, image_name
     
-#     def __len__(self):
-#         return len(self.image_names)
+    def __len__(self):
+        return len(self.image_names)
     
-# def get_transform():
-#     return T.Compose([T.ToTensor()])
+def get_transform():
+    return T.Compose([T.ToTensor()])
 
-# def collate_fn(batch):
-#     return tuple(zip(*batch))
+def collate_fn(batch):
+    return tuple(zip(*batch))
 
-# dataset = FaceMaskDetectionDataset(df, DIR_IMAGES, transforms = get_transform())
+dataset = FaceMaskDetectionDataset(df, DIR_IMAGES, transforms = get_transform())
 
-# indices = torch.randperm(len(dataset)).tolist()
-# train_dataset = torch.utils.data.Subset(dataset, indices[:-866])
-# valid_dataset = torch.utils.data.Subset(dataset, indices[-866:])
+indices = torch.randperm(len(dataset)).tolist()
+train_dataset = torch.utils.data.Subset(dataset, indices[:-866])
+valid_dataset = torch.utils.data.Subset(dataset, indices[-866:])
 
-# print(len(train_dataset))
-# print(len(valid_dataset))
+print(len(train_dataset))
+print(len(valid_dataset))
 
-# train_data_loader = DataLoader(
-#     train_dataset,
-#     batch_size = 4,
-#     shuffle = True,
-#     num_workers = 4,
-#     collate_fn = collate_fn
-# )
+train_data_loader = DataLoader(
+    train_dataset,
+    batch_size = 4,
+    shuffle = True,
+    num_workers = 4,
+    collate_fn = collate_fn
+)
 
-# valid_data_loader = DataLoader(
-#     valid_dataset,
-#     batch_size = 4,
-#     shuffle = True,
-#     num_workers = 4,
-#     collate_fn = collate_fn
-# )
+valid_data_loader = DataLoader(
+    valid_dataset,
+    batch_size = 4,
+    shuffle = True,
+    num_workers = 4,
+    collate_fn = collate_fn
+)
 
-# device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-# torch.cuda.empty_cache()
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+torch.cuda.empty_cache()
 
-# model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=None, weights_backbone = None)
-# num_classes = len(class_to_int)
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=None, weights_backbone = None)
+num_classes = len(class_to_int)
 
-# # get number of input features for the classifier
-# in_features = model.roi_heads.box_predictor.cls_score.in_features
+# get number of input features for the classifier
+in_features = model.roi_heads.box_predictor.cls_score.in_features
 
-# # replace the pre-trained head with a new one
-# model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+# replace the pre-trained head with a new one
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-# params = [p for p in model.parameters() if p.requires_grad]
-# optimizer = torch.optim.SGD(params, lr = 0.005, momentum = 0.9, weight_decay=0.0005)
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(params, lr = 0.005, momentum = 0.9, weight_decay=0.0005)
 
-# #LR
-# #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.1)
+#LR
+#lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.1)
 
-# #Load pre-trained model
-# # checkpoint = torch.load("../input/face-mask-detection-trained-weights/fmd_frcnn_e23.pth")
-# # model.load_state_dict(checkpoint['model_state_dict'])
-# # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-# # epoch = checkpoint['epoch']
-# # loss = checkpoint['loss']
+#Load pre-trained model
+# checkpoint = torch.load("../input/face-mask-detection-trained-weights/fmd_frcnn_e23.pth")
+# model.load_state_dict(checkpoint['model_state_dict'])
+# optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+# epoch = checkpoint['epoch']
+# loss = checkpoint['loss']
 
-# model.to(device)
+model.to(device)
 
-# #No of epochs
-# epochs = 1
+#No of epochs
+epochs = 1
 
-# itr = 1
-# total_train_loss = []
+itr = 1
+total_train_loss = []
 
-# for epoch in range(epochs):
+for epoch in range(epochs):
     
-#     start_time = time.time()
-#     train_loss = []
+    start_time = time.time()
+    train_loss = []
     
-#     #Retriving Mini-batch
-#     for images, targets, image_names in train_data_loader:
+    #Retriving Mini-batch
+    for images, targets, image_names in train_data_loader:
         
-#         #Loading images & targets on device
-#         images = list(image.to(device) for image in images)
-#         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+        #Loading images & targets on device
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
         
-#         #Forward propagation
-#         out = model(images, targets)
-#         losses = sum(loss for loss in out.values())
+        #Forward propagation
+        out = model(images, targets)
+        losses = sum(loss for loss in out.values())
         
-#         #Reseting Gradients
-#         optimizer.zero_grad()
+        #Reseting Gradients
+        optimizer.zero_grad()
         
-#         #Back propagation
-#         losses.backward()
-#         torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
-#         optimizer.step()
+        #Back propagation
+        losses.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+        optimizer.step()
         
-#         #Average loss
-#         loss_value = losses.item()
-#         train_loss.append(loss_value)
+        #Average loss
+        loss_value = losses.item()
+        train_loss.append(loss_value)
         
-#         if itr % 25 == 0:
-#             print(f"\n Iteration #{itr} loss: {out} \n")
+        if itr % 25 == 0:
+            print(f"\n Iteration #{itr} loss: {out} \n")
 
-#         itr += 1
+        itr += 1
     
-#     #lr_scheduler.step()    
+    #lr_scheduler.step()    
     
-#     epoch_train_loss = np.mean(train_loss)
-#     total_train_loss.append(epoch_train_loss)
-#     print(f'Epoch train loss is {epoch_train_loss:.4f}')
+    epoch_train_loss = np.mean(train_loss)
+    total_train_loss.append(epoch_train_loss)
+    print(f'Epoch train loss is {epoch_train_loss:.4f}')
 
     
-#     time_elapsed = time.time() - start_time
-#     print("Time elapsed: ",time_elapsed)
+    time_elapsed = time.time() - start_time
+    print("Time elapsed: ",time_elapsed)
     
-#     torch.save({
-#             'epoch': epoch,
-#             'model_state_dict': model.state_dict(),
-#             'optimizer_state_dict': optimizer.state_dict(),
-#             'loss': epoch_train_loss
-#             }, "checkpoint.pth")
+    torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': epoch_train_loss
+            }, "checkpoint.pth")
